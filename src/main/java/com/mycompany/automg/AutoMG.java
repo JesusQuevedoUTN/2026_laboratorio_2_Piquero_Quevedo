bb81e435e82cc19b3f0b460ee8e038cf82edfd86
Alumno <Alumno@LabE-10.inspt>
2026-05-06 11:34:06 -0300

    Muestra los autos

--- src/main/java/com/mycompany/automg/AutoMG.java
+++ src/main/java/com/mycompany/automg/AutoMG.java
@@ -5,12 +5,15 @@ import java.util.ArrayList;
 public class AutoMG {
 
     public static void main(String[] args) {
-        ArrayList autos = new ArrayList();
+        ArrayList<Auto> autos = new ArrayList();
         
         autos.add(new Auto("Toyota", "Corolla", "Rojo", 20000, new Motor(50000, 1.8f, 140)));
         autos.add(new Auto("Ford", "Focus", "Azul", 22000, new Motor(30000, 2.0f, 170)));
         autos.add(new Auto("Chevrolet", "Cruze", "Blanco", 21000, new Motor(40000, 1.4f, 150)));
         
+        for (int i = 0; i < autos.size(); i++) {
+            autos.get(i).mostrarAuto();
+        }
         
     }
 }
