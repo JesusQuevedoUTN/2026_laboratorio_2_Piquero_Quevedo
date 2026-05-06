6c7c104bad9c9ef67840346224b057d328547331
Alumno <Alumno@LabE-08.inspt>
2026-05-06 12:54:04 -0300

    auto familiar y deportivo

--- src/main/java/com/mycompany/automg/AutoDeportivo.java
+++ src/main/java/com/mycompany/automg/AutoDeportivo.java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.automg;
 
-/**
- *
- * @author Alumno
- */
 public class AutoDeportivo extends Auto{
+    private boolean turbo;
+    private boolean cuatroPorCuatro;
+
+    public AutoDeportivo(String marca, String modelo, String color, double precio, Motor motor, boolean turbo, boolean cuatroPorCuatro) {
+        super(marca, modelo, color, precio, motor);
+        this.turbo = turbo;
+        this.cuatroPorCuatro = cuatroPorCuatro;
+    }
+    
     
 }
