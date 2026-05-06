fd901c785dfc698750b8c5bc9c4b15f99ee82301
Alumno <Alumno@LabE-10.inspt>
2026-05-06 11:23:59 -0300

    Autos creados

--- src/main/java/com/mycompany/automg/AutoMG.java
+++ src/main/java/com/mycompany/automg/AutoMG.java
@@ -1,8 +1,16 @@
 package com.mycompany.automg;
 
+import java.util.ArrayList;
+
 public class AutoMG {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ArrayList autos = new ArrayList();
+        
+        autos.add(new Auto("Toyota", "Corolla", "Rojo", 20000, new Motor(50000, 1.8f, 140)));
+        autos.add(new Auto("Ford", "Focus", "Azul", 22000, new Motor(30000, 2.0f, 170)));
+        autos.add(new Auto("Chevrolet", "Cruze", "Blanco", 21000, new Motor(40000, 1.4f, 150)));
+        
+        
     }
 }
