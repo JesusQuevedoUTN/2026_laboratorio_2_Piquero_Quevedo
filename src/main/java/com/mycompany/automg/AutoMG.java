12b6817c3a48032653147e79c62b46559752d188
Alumno <Alumno@LabE-10.inspt>
2026-05-06 11:49:01 -0300

    Trabajo en el main

--- src/main/java/com/mycompany/automg/AutoMG.java
+++ src/main/java/com/mycompany/automg/AutoMG.java
@@ -1,11 +1,13 @@
 package com.mycompany.automg;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AutoMG {
 
     public static void main(String[] args) {
         ArrayList<Auto> autos = new ArrayList();
+        Scanner scan = new Scanner(System.in);
         
         autos.add(new Auto("Toyota", "Corolla", "Rojo", 20000, new Motor(50000, 1.8f, 140)));
         autos.add(new Auto("Ford", "Focus", "Azul", 22000, new Motor(30000, 2.0f, 170)));
@@ -14,6 +16,12 @@ public class AutoMG {
         for (int i = 0; i < autos.size(); i++) {
             autos.get(i).mostrarAuto();
         }
+        
+        System.out.println("Que auto quiere probar: ");
+        int num = scan.nextInt();
+        System.out.println("Cuanto quiere avanzar: ");
+        int distancia = scan.nextInt();
+        autos.get(num).avanzar(distancia);
         
     }
 }
