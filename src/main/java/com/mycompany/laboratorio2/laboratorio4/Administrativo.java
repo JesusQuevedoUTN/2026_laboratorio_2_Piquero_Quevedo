1b847f2c13ea1038fa9306648107faac1fcba80b
Cheese <jesusquevedomdr@gmail.com>
2026-05-07 16:20:38 -0300

    empleado administrativo

--- src/main/java/com/mycompany/laboratorio2/laboratorio4/Administrativo.java
+++ src/main/java/com/mycompany/laboratorio2/laboratorio4/Administrativo.java
@@ -1,10 +1,24 @@
 package com.mycompany.laboratorio2.laboratorio4;
 
 // Autor: Chisus
+
+import com.mycompany.laboratorio2.Auto;
+import java.util.ArrayList;
+
 // Fecha de creación: 07/05/2026
 
 public class Administrativo extends Empleado{
+ private int horasExtra;
 
+    public Administrativo(String nombre, String apellido, int dni, String email, double sueldoBasico, int horasExtra) {
+        super(nombre, apellido, dni, email, sueldoBasico);
+        this.horasExtra = horasExtra;
+    }
+ 
+    public void cargarAuto(ArrayList<Auto> stock){
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    };
+    
     @Override
     public double calcularSueldo() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
--- src/main/java/com/mycompany/laboratorio2/laboratorio4/Vendedor.java
+++ src/main/java/com/mycompany/laboratorio2/laboratorio4/Vendedor.java
@@ -1,9 +1,23 @@
 package com.mycompany.laboratorio2.laboratorio4;
 
 // Autor: Chisus
-// Fecha de creación: 07/05/2026
+import com.mycompany.laboratorio2.Auto;
+import java.util.ArrayList;
 
-public class Vendedor extends Empleado{
+// Fecha de creación: 07/05/2026
+public class Vendedor extends Empleado {
+
+    private int cantidadDeVentas;
+    private ArrayList<Vendedor> vendedores;
+
+    public Vendedor(String nombre, String apellido, int dni, String email, double sueldoBasico, int cantidadDeVentas) {
+        super(nombre, apellido, dni, email, sueldoBasico);
+        this.cantidadDeVentas = cantidadDeVentas;
+    }
+
+    public void venderAuto(ArrayList<Auto> stock, ArrayList<Auto> vendidos) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 
     @Override
     public double calcularSueldo() {
