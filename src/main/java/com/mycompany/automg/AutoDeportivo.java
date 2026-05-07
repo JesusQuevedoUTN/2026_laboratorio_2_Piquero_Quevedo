a38a22998f9dc75093651bf09abf139f19491a39
Cheese <jesusquevedomdr@gmail.com>
2026-05-07 15:35:33 -0300

    auto utilitario completo

--- src/main/java/com/mycompany/automg/AutoDeportivo.java
+++ src/main/java/com/mycompany/automg/AutoDeportivo.java
@@ -1,13 +1,40 @@
 package com.mycompany.automg;
 
-public class AutoDeportivo extends Auto{
-    private boolean turbo;
-    private boolean cuatroPorCuatro;
+import javax.swing.JOptionPane;
 
-    public AutoDeportivo(String marca, String modelo, String color, double precio, Motor motor, boolean turbo, boolean cuatroPorCuatro) {
+public class AutoDeportivo extends Auto{
+    private double velociadaMaxima;
+    private double aceleracionACien;
+
+    public AutoDeportivo(String marca, String modelo, String color, double precio, Motor motor, double velociadaMaxima, double aceleracionACien) {
         super(marca, modelo, color, precio, motor);
-        this.turbo = turbo;
-        this.cuatroPorCuatro = cuatroPorCuatro;
+        this.velociadaMaxima = velociadaMaxima;
+        this.aceleracionACien = aceleracionACien;
+    }
+
+    public double getVelociadaMaxima() {
+        return velociadaMaxima;
+    }
+
+    public void setVelociadaMaxima(double velociadaMaxima) {
+        this.velociadaMaxima = velociadaMaxima;
+    }
+
+    public double getAceleracionACien() {
+        return aceleracionACien;
+    }
+
+    public void setAceleracionACien(double aceleracionACien) {
+        this.aceleracionACien = aceleracionACien;
+    }
+
+    
+    
+    
+    
+    @Override
+    public String toString() {
+        return super.toString() + ", AutoDeportivo{" + "velociadaMaxima=" + velociadaMaxima + ", aceleracionACien=" + aceleracionACien + '}';
     }
     
     
--- src/main/java/com/mycompany/automg/AutoUtilitario.java
+++ src/main/java/com/mycompany/automg/AutoUtilitario.java
@@ -1,7 +1,13 @@
 package com.mycompany.automg;
 
+import javax.swing.JOptionPane;
+
 
 public class AutoUtilitario extends Auto{
+    
+    private final double MAXIMA_CARGA   = 4000;
+    private final double MAXIMO_VOLUMEN = 20;
+    
     private double capacidadDeCarga;
     private double volumenCarga;
 
@@ -9,6 +15,40 @@ public class AutoUtilitario extends Auto{
         this.capacidadDeCarga = capacidadDeCarga;
         this.volumenCarga = volumenCarga;
     }
+
+    public double getCapacidadDeCarga() {
+        return capacidadDeCarga;
+    }
+
+    public void setCapacidadDeCarga(double capacidadDeCarga) {
+        this.capacidadDeCarga = capacidadDeCarga;
+    }
+
+    public double getVolumenCarga() {
+        return volumenCarga;
+    }
+
+    public void setVolumenCarga(double volumenCarga) {
+        this.volumenCarga = volumenCarga;
+    }
+
+    @Override
+    public void avanzar(int km){
+        if(this.capacidadDeCarga >= MAXIMA_CARGA){
+            JOptionPane.showMessageDialog(null, "El camión no puede llevar algo tan pesado.");
+        }else if (this.volumenCarga >= MAXIMO_VOLUMEN){
+            JOptionPane.showMessageDialog(null, "El camión no puede llevar algo tan grande.");
+        }else{
+            super.avanzar(km);
+        }
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString() + ", AutoUtilitario{" + "capacidadDeCarga=" + capacidadDeCarga + ", volumenCarga=" + volumenCarga + '}';
+    }
+    
+    
     
     
 }
