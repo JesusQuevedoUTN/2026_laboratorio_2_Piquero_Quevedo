e047e94646dc68b1fadea7ff53d9bae8dbc0415b
Cheese <jesusquevedomdr@gmail.com>
2026-05-07 16:14:49 -0300

    empleado gerente

--- src/main/java/com/mycompany/laboratorio2/laboratorio4/Gerente.java
+++ src/main/java/com/mycompany/laboratorio2/laboratorio4/Gerente.java
@@ -1,8 +1,32 @@
 package com.mycompany.laboratorio2.laboratorio4;
 
 // Autor: Chisus
+
+import java.util.ArrayList;
+
 // Fecha de creación: 07/05/2026
 
-public class Gerente{
+public class Gerente extends Empleado{
+    private double bono;
+    private ArrayList<Administrativo> administrativos;
+    private ArrayList<Vendedor> vendedores;
+     
+    public Gerente(String nombre, String apellido, int dni, String email, double sueldoBasico, double bono) {
+        super(nombre, apellido, dni, email, sueldoBasico);
+        this.bono = bono;
+        this.administrativos = new ArrayList<Administrativo>();
+        this.vendedores = new ArrayList<Vendedor>();
+    }
+    
+    public void agregarEmpleado(ArrayList<Administrativo> administradores, ArrayList<Vendedor> ventas){
+        this.administrativos = administradores;
+        this.vendedores = ventas;
+    };
 
+    @Override
+    public double calcularSueldo() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
 }
--- src/main/java/com/mycompany/laboratorio2/laboratorio4/Vendedor.java
+++ src/main/java/com/mycompany/laboratorio2/laboratorio4/Vendedor.java
@@ -5,4 +5,9 @@ package com.mycompany.laboratorio2.laboratorio4;
 
 public class Vendedor extends Empleado{
 
+    @Override
+    public double calcularSueldo() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
 }
